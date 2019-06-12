2b7ae0547e057a44f44a8e59e14f61fb7a33ac43
mrOdinson <cankirsallioba@gmail.com>
2019-06-12 11:03:54 +0300

    Elements and Buttons class

---
+++ src/Elements.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.util.Map;
+
+/**
+ * This class contains the elements for the calculator.
+ * @author cankirsallioba
+ * @version 12.06.2019
+ */
+public class Elements extends JFrame {
+
+    //Properties
+    Map <String, Buttons> buttonMap;
+    private JPanel numPad;
+
+
+
+}
