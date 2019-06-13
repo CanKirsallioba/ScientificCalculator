4b0f430c9f59a5a42b64384e8d88232465008689
mrOdinson <cankirsallioba@gmail.com>
2019-06-13 19:27:57 +0300

    Still gives null pointer exception

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
-    public static void main (String args []) {
+    public static void main (String [] args) {
         CalculatorLogic calculator = new CalculatorLogic ( 450, 600 );
         calculator.setTitle ("CK-Calculator");
         calculator.getContentPane ().setBackground ( FontAndColours.BACKGROUND_COLOR);
         calculator.setLocationRelativeTo ( null );
         calculator.setVisible ( true );
-        calculator.setIcon (  );
+        //calculator.setIcon (  );
     }
 }
