5625fb81a3d73ee59af6ba594faf910748f13226
mrOdinson <cankirsallioba@gmail.com>
2019-06-13 10:07:11 +0300

    Buttons class added

--- src/Buttons.java
+++ src/Buttons.java
@@ -1,2 +1,21 @@
-public class Buttons {
+import javax.swing.*;
+
+public class Buttons extends JButton {
+    String buttonType;
+    String nameOfElement;
+    String message;
+
+    private Buttons(String buttonType, String nameOfElement, String buttonText, String message) {
+        super(buttonText);
+        this.buttonType = buttonType;
+        this.nameOfElement = nameOfElement;
+        this.message = message;
+    }
+
+    public String toString() {
+        return "{ButtonType: " + this.buttonType + ", nameOfElement: " + this.nameOfElement + ", button:" + this.getText() + ", message: " + this.message + "}";
+    }
+
+    public boolean isOperator
+
 }
