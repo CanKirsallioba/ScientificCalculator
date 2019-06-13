13b89d7d7fa83ecc96d2401e0aec2f3186e73154
mrOdinson <cankirsallioba@gmail.com>
2019-06-13 18:35:01 +0300

    Nearly finished

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,10 @@
 public class Main {
-    CalculatorLogic calculatorLogic = new CalculatorLogic (450, 600);
+    public static void main (String args []) {
+        CalculatorLogic calculator = new CalculatorLogic ( 450, 600 );
+        calculator.setTitle ("CK-Calculator");
+        calculator.getContentPane ().setBackground ( FontAndColours.BACKGROUND_COLOR);
+        calculator.setLocationRelativeTo ( null );
+        calculator.setVisible ( true );
+        calculator.setIcon (  );
+    }
 }
